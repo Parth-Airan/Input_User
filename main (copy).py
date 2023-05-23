c5f68458f40f99258629191d4df8772e556df649
Parth-Airan <132052702+Parth-Airan@users.noreply.github.com>
2023-05-23 16:29:22 +0530

    Input_User
    About code use for input of user in an app

---
+++ main (copy).py
@@ -0,0 +1,11 @@
+Name = input("Enter your name: ")
+password = input("Enter your password: ")
+
+if Name == "Parth":
+   print("Hi" +  Name)
+
+elif password == "Parth_//":
+  print("Hi" +  Name)
+
+else :
+   print("Invalid Name or Password")
